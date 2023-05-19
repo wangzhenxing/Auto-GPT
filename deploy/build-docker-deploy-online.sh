6fada7b6fced61b3471e7ee515d95e36e0f13d52
sky <wzx1987310@gmail.com>
2023-05-19 15:14:43 +0800

    增加deploy

--- deploy/build-docker-deploy-online.sh
+++ deploy/build-docker-deploy-online.sh
@@ -2,9 +2,16 @@
 set -x
 
 VERSION="v"$(date "+%Y%m%d%H%M%S")
+cd ../
 docker build -f Dockerfile -t autogpt:${VERSION} .
+cd -
 
-cd /root/Deploy/autogpt/autogpt_backend
+ONLINE_DIR=/root/Deploy/autogpt/autogpt_backend
+
+cp docker-componse.yaml ${ONLINE_DIR}
+cp envoy.yaml ${ONLINE_DIR}
+
+cd ${ONLINE_DIR}
 sed -i "/AUTOGPT_VERSION/s/AUTOGPT_VERSION.*/AUTOGPT_VERSION=${VERSION}/g" .env
 cat .env
 
